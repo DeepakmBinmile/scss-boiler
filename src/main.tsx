68fc2f02af3d47ee40152ba0bad3f6e02901e74a
Jayant Rohila <jayant@binmile.com>
2024-09-23 18:03:48 +0530

    fixed error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') || document.createElement('div')).render(
   <StrictMode>
     <App />
   </StrictMode>,
