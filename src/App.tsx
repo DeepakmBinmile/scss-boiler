cdd1139abccf6e1bb7dafa837227338143cc968f
aakansha183 <aakansha.tyagi@binmile.com>
2024-09-25 17:43:42 +0530

    Component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Button from './components/button/button';
+import UserIcon from './assets/imagesData/UserIcon';
 
 function App() {
   return (
     <>
-      <Button>lol</Button>
+      <Button>Default Button</Button>
+      <Button>Next</Button>
+      <Button variant='secondary'>Button with secondary styling</Button>
+      <Button size='large'>Button with large Size</Button>
+      <Button size='small' variant='secondary'>Button with small Size</Button>
+
+      <Button icon={<UserIcon />} size='medium' variant='secondary'>
+        Button with Icon
+      </Button>
+     
     </>
   );
 }
 
 export default App;
+
+
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,24 @@
 import { forwardRef } from 'react';
 import './button.scss';
+
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
   size?: 'small' | 'medium' | 'large';
+  icon?: React.ReactNode;
 }
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ variant = 'primary', size = 'medium', ...props }, ref) => (
-  <button ref={ref} className={`custom-button ${variant} ${size}`} {...props} />
-));
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant = 'primary', size = 'medium', icon, children, ...props }, ref) => (
+    <button ref={ref} className={`custom-button ${variant} ${size}`} {...props}>
+      {icon && <span className="button-icon">{icon}</span>}
+      {children}
+    </button>
+  )
+);
 
 Button.displayName = 'Button';
 
 export default Button;
+
+
+
