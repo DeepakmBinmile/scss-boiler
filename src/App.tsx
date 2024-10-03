b5b576f9a619b1b9a8e25923c7dbb5ad91733557
Abhijeet <abhijeet@Mac-mini.local>
2024-10-03 16:07:22 +0530

    autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
 import ThemeProvider from './context/theme-provider';
 import { ModeToggle } from './components/theme-toggle/mode-toggle';
+import ExampleAutoComplete from './components/mui-custom-autoComplete/ExampleAutoComplete';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div>
           <ModeToggle />
           <h1>Hi check the scss here</h1>
+          <ExampleAutoComplete />
         </div>
       </Provider>
     </ThemeProvider>
